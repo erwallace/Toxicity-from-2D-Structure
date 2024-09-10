36aa805ec88849f5f18af1ecdace12f39916fd4d
Ewan Wallace <ewan.wallace@roche.com>
2024-09-10 09:22:12 +0100

    simplified code

--- src/transforms/_mol_to_greyscale.py
+++ src/transforms/_mol_to_greyscale.py
@@ -33,29 +33,26 @@ def mol_to_greyscale(mol: Chem.Mol, embed: float = 20.0, res: float = 0.5) -> np
     np.array
         The greyscale image of the molecule.
     """
-    dims = int(embed * 2 / res)
-    cmol = Chem.Mol(mol.ToBinary())
-    AllChem.Compute2DCoords(cmol)
-    coords = cmol.GetConformer(0).GetPositions()
-    vect = np.zeros((dims, dims, 1))
+    len_ = int(embed * 2 / res)
+    AllChem.Compute2DCoords(mol)
+    coords = mol.GetConformer(0).GetPositions()
+    vect = np.zeros((len_, len_, 1))
 
     # Bonds first
     for i, bond in enumerate(mol.GetBonds()):
         bondorder = bond.GetBondTypeAsDouble()
-        bidx = bond.GetBeginAtomIdx()
-        eidx = bond.GetEndAtomIdx()
-        bcoords = coords[bidx]
-        ecoords = coords[eidx]
+        start_coords = coords[bond.GetBeginAtomIdx()]
+        end_coords = coords[bond.GetEndAtomIdx()]
         frac = np.linspace(0, 1, int(1 / res * 2))
 
         for f in frac:
-            c = f * bcoords + (1 - f) * ecoords
+            c = f * start_coords + (1 - f) * end_coords
             idx = int(round((c[0] + embed) / res))
             idy = int(round((c[1] + embed) / res))
             vect[idx, idy, 0] = bondorder
 
     # Atoms second
-    for i, atom in enumerate(cmol.GetAtoms()):
+    for i, atom in enumerate(mol.GetAtoms()):
         idx = int(round((coords[i][0] + embed) / res))
         idy = int(round((coords[i][1] + embed) / res))
         # Atomic number
