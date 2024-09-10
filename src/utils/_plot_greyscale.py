69d9c3d85214e962e1138f5e01ecb8b1753d7884
Ewan Wallace <ewan.wallace@roche.com>
2024-09-10 09:01:13 +0100

    refactored to make more robust

--- src/utils/_plot_greyscale.py
+++ src/utils/_plot_greyscale.py
@@ -9,8 +9,32 @@ def plot_greyscale(arr: np.array):
     ---------
     arr : np.array
         A 4D array with shape (batch_size, height, width, channels).
+        OR
+        A 3D array with shape (height, width, channels).
     """
-    for i in range(arr.shape[0]):
+
+    def greyscale(arr):
+        """Plot a greyscale figure.
+
+        Parameter
+        ---------
+        arr : np.array
+            A 3D array with shape (height, width, channels).
+        """
         fig, ax = plt.subplots(figsize=(10, 5))
-        plt.imshow(arr[i, :], cmap="Greys")
+        plt.imshow(arr, cmap="Greys")
         plt.show()
+
+    assert arr.shape[-1] == 1, "The array must have only one channel."
+
+    if arr.dim() == 3:
+        greyscale(arr)
+
+    elif arr.dim() == 4:
+        for i in range(arr.shape[0]):
+            greyscale(arr[i, :])
+
+    else:
+        raise ValueError(
+            f"The array has {arr.dim()} dimensions ({arr.shape}). Must have 3 or 4."
+        )
