a4e880e34c6718b2a07202deb7c695e096906c53
Ewan Wallace <ewan.wallace@roche.com>
2024-09-25 15:36:01 +0100

    fixed typing

--- src/transforms/_smiles_to_mol.py
+++ src/transforms/_smiles_to_mol.py
@@ -1,7 +1,6 @@
 from rdkit import Chem
-from typing import Any
 
 
 class SMILESToMol:
-    def __call__(self, feature: str) -> tuple[Chem.Mol, Any]:
-        return Chem.MolFromSmiles(feature)
+    def __call__(self, smile: str) -> Chem.Mol:
+        return Chem.MolFromSmiles(smile)
