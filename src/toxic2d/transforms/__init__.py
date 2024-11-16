494939b33353cafe7e1ef9a4298a93ed53582b6e
Ewan Wallace <ewan.wallace@roche.com>
2024-11-16 22:47:09 +0000

    morgan fingerprint transform

--- src/toxic2d/transforms/__init__.py
+++ src/toxic2d/transforms/__init__.py
@@ -1,5 +1,11 @@
 from ._mol_to_greyscale import MolToGreyscale
+from ._mol_to_morgan_fingerprints import MolToMorganFingerprints
 from ._mol_to_rdkit_descriptors import MolToRDKitDescriptors
 from ._smiles_to_mol import SMILESToMol
 
-__all__ = ["SMILESToMol", "MolToGreyscale", "MolToRDKitDescriptors"]
+__all__ = [
+    "SMILESToMol",
+    "MolToGreyscale",
+    "MolToRDKitDescriptors",
+    "MolToMorganFingerprints",
+]
