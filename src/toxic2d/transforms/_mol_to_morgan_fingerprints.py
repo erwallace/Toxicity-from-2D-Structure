50ea72780f5c079dec653a586e092bd56bbdca57
Ewan Wallace <ewan.wallace@roche.com>
2024-11-16 23:06:35 +0000

    corrected kwargs

--- src/toxic2d/transforms/_mol_to_morgan_fingerprints.py
+++ src/toxic2d/transforms/_mol_to_morgan_fingerprints.py
@@ -7,18 +7,18 @@ class MolToMorganFingerprints:
     def __init__(
         self,
         radius: int = 2,
-        n_bits: int = 256,
+        fpSize: int = 256,
         includeChirality: bool = True,
         use_counts: bool = True,
     ) -> None:
         self.radius = radius
-        self.n_bits = n_bits
+        self.fpSize = fpSize
         self.includeChirality = includeChirality
         self.use_counts = use_counts
 
     def __call__(self, mol: Chem.Mol) -> torch.Tensor:
         generator = rdFingerprintGenerator.GetMorganGenerator(
-            radius=self.radius, nBits=self.n_bits, includeChirality=self.includeChirality
+            radius=self.radius, fpSize=self.fpSize, includeChirality=self.includeChirality
         )
         if self.use_counts:
             fp = generator.GetCountFingerprintAsNumPy(mol)
