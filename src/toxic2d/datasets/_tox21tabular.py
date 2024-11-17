bbefb966775b9c514a1d5b94d74cc8de00d40d40
Ewan Wallace <ewan.wallace@roche.com>
2024-11-17 10:38:18 +0000

    split out scaling and removing transformed nan values

--- src/toxic2d/datasets/_tox21tabular.py
+++ src/toxic2d/datasets/_tox21tabular.py
@@ -7,11 +7,26 @@ from toxic2d.datasets import Tox21Base
 
 
 class Tox21Tabular(Tox21Base):
-    def __init__(self, csv_path, transform=None, target_transform=None):
+    def __init__(
+        self,
+        csv_path: str,
+        transform: list = [],
+        target_transform: list = [],
+        scaling: bool = True,
+        remove_tranformed_nan: bool = True,
+    ):
         super().__init__(csv_path, transform, target_transform)
-        self.scaler = None
         self.transformed_features = None
-        self.scaler_fit()
+
+        if scaling and not remove_tranformed_nan:
+            raise ValueError("NaN removal must be enabled when scaling is enabled.")
+
+        if remove_tranformed_nan:
+            self.remove_tranformed_nan()
+
+        if scaling:
+            self.scaler = None
+            self.scaler_fit()
 
     def __getitem__(self, idx: int) -> tuple:
         features = self.data["smiles"].iloc[idx]
@@ -22,30 +37,29 @@ class Tox21Tabular(Tox21Base):
         if self.target_transform:
             label = self.target_transform(label)
 
-        features = self.scaler_transform(features)
+        if self.scaling:
+            features = self.scaler_transform(features)
 
         return features, label
 
-    def scaler_fit(self):
-        """Fits the StandardScaler to the dataset.
-
-        To do this, we first apply the transformation to the entire dataset, remove any
-        nan rows that the transformations create and then fit the scaler to the
-        transformed data.
-        """
+    def remove_tranformed_nan(self):
+        """Removes any rows that contain NaN values after the transformation."""
         # TODO: is there a better way of doing this? even with only 7,000 datapoints its slow
         if self.transform:
             transformed_smiles = [self.transform(smile) for smile in self.data["smiles"]]
             self.transformed_features = torch.stack(transformed_smiles).numpy()
 
-            # Replace np.inf with np.nan
+            # Replace remove np.inf and np.nan rows
             self.transformed_features[np.isinf(self.transformed_features)] = np.nan
-
             nan_idxs = np.where(np.isnan(self.transformed_features).any(axis=1))[0]
-            self.scaler = StandardScaler().fit(self.transformed_features[~nan_idxs])
+            self.transformed_features = self.transformed_features[~nan_idxs]
 
             print(f"Removing {len(nan_idxs)} NaN rows from the dataset")
             self.data = self.data.drop(nan_idxs).reset_index(drop=True)
+
+    def scaler_fit(self):
+        """Fits the StandardScaler to the dataset. Must be called after remove_tranformed_nan."""
+        self.scaler = StandardScaler().fit(self.transformed_features)
 
     def scaler_transform(self, features):
         """Uses sklearn scaler to transform data. Must be called after scale_fit."""
